95d1306b71a115bfaf174f678f306f879c8c6780
AleksandrBara <alexbar130379@gmail.com>
2022-07-27 12:02:24 +0300

    Добавил InlineKeyboardButton к вопросу о Электронике.

--- telegram_bot.py
+++ telegram_bot.py
@@ -27,14 +27,27 @@ def start_message(message):
 
 @bot.message_handler(content_types='text')
 def message_reply(message):
-    if message.text == "Где у Электроника кнопка?":
-        bot.send_message(message.chat.id, "https://www.ivi.ru/watch/priklucheniya-elektronika/6291")
     if message.text == "Отправить письмо Лектору":
         bot.send_message(message.chat.id, "Ури, Сегодня только фуршет!!!)))")
     if message.text == "Посмотпеть расписание":
         bot.send_message(message.chat.id, text=timetable)
     if message.text == "Спикеры на сегодня":
         bot.send_message(message.chat.id, text='Список пока не определен')
+    if message.text == "Где у Электроника кнопка?":
+        keyboard = types.InlineKeyboardMarkup()
+        key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
+        keyboard.add(key_yes)
+        key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
+        keyboard.add(key_no)
+        bot.send_message(message.from_user.id, text="Хочешь ссылку на фильм?", reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes":
+        bot.send_message(call.message.chat.id, "https://www.ivi.ru/watch/priklucheniya-elektronika/6291")
+    elif call.data == "no":
+        bot.send_message(call.message.chat.id, "Не забудь посмотреть потом!!")
 
 
 bot.infinity_polling()
