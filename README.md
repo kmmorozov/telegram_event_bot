aa79d6388d9107cf5433436408cd778229723703
astershch <107745329+astershch@users.noreply.github.com>
2022-07-27 19:34:58 +0400

    some fixes
    - edit README
    - delete commernts
    - change requirements

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # telebot_template
- Template for synchronous implementation of telebot
 
- You are welcome to make pull requests or open issues to improve template.
+## Использование
 
- Note that this template requires pyTelegramBotAPI version higher or equal to 4.4.0. You can install it from github because it is not released yet.
+Запуск через `bot.py`. Все зависимости в requirements.txt
+
+Нужно добавить токен бота в .env
--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -4,16 +4,6 @@ from tgbot.handlers.bot_commands import user_commands
 
 
 def start_command(message: types.Message, bot: TeleBot):
-    # item1 = types.InlineKeyboardButton("Описание события", callback_data='event')
-    # item2 = types.InlineKeyboardButton("Задать вопрос докладчику", callback_data='ask_speaker')
-    # item3 = types.InlineKeyboardButton("Программа мероприятия", callback_data='event_program')
-    #
-    # keyboard = types.InlineKeyboardMarkup()
-    # keyboard.add(item1)
-    # keyboard.add(item2)
-    # keyboard.add(item3)
-    # bot.send_message(message.chat.id, text='Пользователь', reply_markup=keyboard)
-    # bot.send_message(message.chat.id, "Event info")
     commands = []
 
     for command, description in user_commands.items():
