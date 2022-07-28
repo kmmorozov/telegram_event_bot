e7d1f6664f75184140c1011ad1f9f69813d5cc11
astershch <107745329+astershch@users.noreply.github.com>
2022-07-28 18:32:15 +0400

    добавил меню и сообщение спикеру

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,20 @@
-import telebot
-from telebot import types
-from dotenv import load_dotenv
+import json
 import os
+
+from dotenv import load_dotenv
+from telebot import TeleBot
+from telebot import types
+from telebot.apihelper import ApiTelegramException
+
+
+NOT_FOUND_MESSAGE = 'Информация не найдена'
+
+with open('./example.json', 'rb') as file:
+    events_dict = json.load(file)
 
 load_dotenv()
 token = os.getenv("TELEGRAM_TOKEN")
-bot = telebot.TeleBot(token)
+bot = TeleBot(token)
 timetable = 'Следующая лекция в сентябре!!'
 
 
@@ -13,46 +22,146 @@ timetable = 'Следующая лекция в сентябре!!'
 def start_message(message):
     user_id = message.from_user.id
     user_name = message.from_user.username
-    bot.send_message(message.chat.id, 'Привет')
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Посмотреть расписание")
-    markup.add(item1)
-    item2 = types.KeyboardButton("Отправить письмо Лектору")
-    markup.add(item2)
-    item3 = types.KeyboardButton("Спикеры на сегодня")
-    markup.add(item3)
-    item4 = types.KeyboardButton("Где у Электроника кнопка?")
-    markup.add(item4)
-    print(user_name)
-    print(user_id)
 
-    bot.send_message(message.chat.id, 'Выберите нужный пункт меню', reply_markup=markup)
+    bot_command = types.BotCommand('start', 'Стартовая страница')
+    command_scope = types.BotCommandScopeChat(message.chat.id)
+    bot.set_my_commands([bot_command], command_scope)
 
-
-@bot.message_handler(content_types='text')
-def message_reply(message):
-    if message.text == "Отправить письмо Лектору":
-        bot.send_message(message.chat.id, "Ури, Сегодня только фуршет!!!)))")
-    if message.text == "Посмотреть расписание":
-        bot.send_message(message.chat.id, text=timetable)
-    if message.text == "Спикеры на сегодня":
-        bot.send_message(message.chat.id, text='Список пока не определен')
-    if message.text == "Где у Электроника кнопка?":
-        keyboard = types.InlineKeyboardMarkup()
-        key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
-        keyboard.add(key_yes)
-        key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
-        keyboard.add(key_no)
-
-        bot.send_message(message.from_user.id, text="Хочешь ссылку на фильм?", reply_markup=keyboard)
+    button = types.InlineKeyboardButton('Главное меню', callback_data='menu')
+    markup = types.InlineKeyboardMarkup()
+    markup.add(button)
+    bot.send_message(message.chat.id, reply_markup=markup, text='Привет!\nИнфо о боте')
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    if call.data == "yes":
-        bot.send_message(call.message.chat.id, "https://www.ivi.ru/watch/priklucheniya-elektronika/6291")
-    elif call.data == "no":
-        bot.send_message(call.message.chat.id, "Не забудь посмотреть потом!!")
+    markup = types.InlineKeyboardMarkup()
+
+    if call.data == "program":
+
+        for event in events_dict:
+            markup.add(
+                types.InlineKeyboardButton(
+                    event['name'],
+                    callback_data=f'program_{event["id"]}'
+                )
+            )
+
+        markup.add(types.InlineKeyboardButton('Главное меню', callback_data='menu'))
+
+        bot.send_message(call.message.chat.id, reply_markup=markup, text='Программа')
+
+    elif call.data == "menu":
+        button1 = types.InlineKeyboardButton('Программа', callback_data='program')
+        button2 = types.InlineKeyboardButton('Задать вопрос спикеру', callback_data='ask_speaker')
+
+        markup.add(button1, button2)
+
+        bot.send_message(call.message.chat.id, reply_markup=markup, text='Главное меню')
+
+    elif call.data == 'ask_speaker':
+        for event in events_dict:
+            markup.add(
+                types.InlineKeyboardButton(
+                    event['name'],
+                    callback_data=f'ask_speaker_{event["id"]}'
+                )
+            )
+
+        markup.add(types.InlineKeyboardButton('Главное меню', callback_data='menu'))
+
+        bot.send_message(call.message.chat.id, reply_markup=markup, text='Задать вопрос спикеру')
+
+    else:
+        for event in events_dict:
+            if call.data == f'program_{event["id"]}':
+                for block in event['blocks']:
+                    markup.add(
+                        types.InlineKeyboardButton(
+                            block['name'],
+                            callback_data=f'program_{block["id"]}'
+                        )
+                    )
+
+                markup.add(types.InlineKeyboardButton('Назад', callback_data='program'))
+                bot.send_message(call.message.chat.id, reply_markup=markup, text=event['name'])
+
+            elif call.data == f'ask_speaker_{event["id"]}':
+                for block in event['blocks']:
+                    markup.add(
+                        types.InlineKeyboardButton(
+                            f'{block["start_time"]} - {block["end_time"]}',
+                            callback_data=f'ask_speaker_{block["id"]}'
+                        )
+                    )
+
+                markup.add(types.InlineKeyboardButton('Назад', callback_data='ask_speaker'))
+                bot.send_message(call.message.chat.id, reply_markup=markup, text=event['name'])
+
+            for block in event['blocks']:
+                if call.data == f'program_{block["id"]}':
+                    markup.add(types.InlineKeyboardButton('Назад', callback_data=f'program_{event["id"]}'))
+                    text = block.get('text') or NOT_FOUND_MESSAGE
+                    bot.send_message(call.message.chat.id, reply_markup=markup, text=text)
+
+                elif call.data == f'ask_speaker_{block["id"]}':
+                    for speaker in block['speakers']:
+                        markup.add(
+                            types.InlineKeyboardButton(
+                                f'{speaker["speaker_id"]} - {speaker["name"]}',
+                                callback_data=speaker["id"]
+                            )
+                        )
+
+                    markup.add(types.InlineKeyboardButton('Назад', callback_data=f'ask_speaker_{event["id"]}'))
+                    bot.send_message(
+                        call.message.chat.id,
+                        reply_markup=markup,
+                        text=f'{block["start_time"]} - {block["end_time"]}'
+                    )
+
+                if block['speakers']:
+                    for speaker in block['speakers']:
+                        if call.data == speaker['id']:
+                            speaker_answer = bot.send_message(call.message.chat.id, text='Введите вопрос спикеру:')
+                            bot.register_next_step_handler(speaker_answer, confirm_send_message, speaker['speaker_id'])
+
+
+def confirm_send_message(message: types.Message, speaker_id):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton('Да')
+    button2 = types.KeyboardButton('Нет')
+    markup.add(button1, button2)
+
+    confirmation = bot.send_message(message.chat.id, text='Отправить сообщение?', reply_markup=markup)
+    bot.register_next_step_handler(confirmation, send_message_to_speaker, message, speaker_id)
+
+
+def send_message_to_speaker(message, answer, chat_id):
+    if message.text == 'Да':
+        try:
+            bot.send_message(
+                chat_id,
+                f'From: @{answer.from_user.username}\n{answer.text}'
+            )
+            bot.send_message(
+                message.chat.id,
+                'Сообщение отправлено',
+                reply_markup=types.ReplyKeyboardRemove()
+            )
+        except ApiTelegramException:
+            bot.send_message(
+                message.chat.id,
+                'Ошибка сервера. \nПожалуйста, повторите позднее.',
+                reply_markup=types.ReplyKeyboardRemove()
+            )
+
+    elif message.text == 'Нет':
+        bot.send_message(
+            message.chat.id,
+            'Сообщение не отправлено',
+            reply_markup=types.ReplyKeyboardRemove()
+        )
 
 
 bot.infinity_polling()
