057b30d9cc027a1ff30a109515ef7dde219f9a77
astershch <107745329+astershch@users.noreply.github.com>
2022-08-01 17:05:10 +0400

    create user`s payment value

--- bot.py
+++ bot.py
@@ -1,20 +1,22 @@
 import json
 import os
 import re
-from telebot.types import LabeledPrice, ShippingOption
+
 from dotenv import load_dotenv
+from telebot.types import LabeledPrice
 from telebot import TeleBot
 from telebot import types
 from telebot.apihelper import ApiTelegramException
 
-provider_token = '1832575495:TEST:01cba465218af40acc25feac199f050b537560e46d82096d28b94a376120ad8c'
+
 NOT_FOUND_MESSAGE = 'Информация не найдена'
 
 with open('./example.json', 'rb') as file:
     events_dict = json.load(file)
 
 load_dotenv()
-token = os.getenv("TELEGRAM_TOKEN")
+token = os.getenv('TELEGRAM_TOKEN')
+provider_token = os.getenv('PROVIDER_TOKEN')
 bot = TeleBot(token)
 
 
@@ -70,42 +72,26 @@ def callback_worker(call):
         bot.send_message(call.message.chat.id, reply_markup=markup, text='Задать вопрос спикеру')
 
     elif call.data == 'donate':
-        # button1 = types.InlineKeyboardButton('100', callback_data='100')
-        # button3 = types.InlineKeyboardButton('500', callback_data='500')
-        # button2 = types.InlineKeyboardButton('1000', callback_data='1000')
-        # button4 = types.InlineKeyboardButton('10000', callback_data='10000')
-        # button5 = types.InlineKeyboardButton('Вернуться в главное меню', callback_data='menu')
-        # markup.add(button1, button2, button3, button4, button5, row_width=2)
-        # bot.send_message(call.message.chat.id, reply_markup=markup, text='На какую суммубудет донат?')
-        # print(call.message.chat.id)
-        prices = [LabeledPrice(label='На развитие', amount=5678990)]
-        bot.send_invoice(
-            call.message.chat.id,  # chat_id
-            'На развитие бота',  # title
-            'Вы донатите на развитие нашего бота и нашего бесплатного MeetUP',
-            'HAPPY FRIDAYS COUPON',  # invoice_payload
-            provider_token,  # provider_token
-            'rub',
-            prices,
-            is_flexible=False,
-            start_parameter='time-machine-example')
+        button1 = types.InlineKeyboardButton('100', callback_data='pay_100')
+        button3 = types.InlineKeyboardButton('500', callback_data='pay_500')
+        button2 = types.InlineKeyboardButton('1000', callback_data='pay_1000')
+        button4 = types.InlineKeyboardButton('10000', callback_data='pay_10000')
+        button5 = types.InlineKeyboardButton('Ввести сумму', callback_data='pay_user')
+        button6 = types.InlineKeyboardButton('Главное меню', callback_data='menu')
+        markup.add(button1, button2, button3, button4, button5, button6, row_width=2)
+        bot.send_message(call.message.chat.id, reply_markup=markup, text='Выберите сумму')
 
-        @bot.pre_checkout_query_handler(func=lambda query: True)
-        def checkout(pre_checkout_query):
-            bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True,
-                                          error_message="Попробуйте еще раз оплатить через несколько минут.")
+    elif rexp := re.search(r'^id_(\d+)$', call.data):
+        answer_to_user = bot.send_message(call.message.chat.id, text='Введите ответ пользователю:')
+        bot.register_next_step_handler(answer_to_user, confirm_send_message, rexp.group(1), 'user')
 
-        @bot.message_handler(content_types=['successful_payment'])
-        def got_payment(message):
-            bot.send_message(message.chat.id,
-                             'Спасибо за ваш донат! Ваше пожертвование в размере `{} {}` пойдет на добрые дела! '
-                             .format(
-                                 message.successful_payment.total_amount / 100, message.successful_payment.currency),
-                             parse_mode='Markdown')
+    elif rexp := re.search(r'^pay_(\d+)$', call.data):
+        pay_donate(call.message, rexp.group(1))
 
-        markup.add(types.InlineKeyboardButton('Главное меню', callback_data='menu'))
+    elif call.data == 'pay_user':
+        pay_user_message = bot.send_message(call.message.chat.id, text='Введите сумму:')
+        bot.register_next_step_handler(pay_user_message, pay_donate, 'user')
 
-        bot.send_message(call.message.chat.id, reply_markup=markup, text='Или вернуться в главное меню.')
     else:
         for event in events_dict:
             if call.data == f'program_{event["id"]}':
@@ -157,42 +143,26 @@ def callback_worker(call):
                 if block['speakers']:
                     for speaker in block['speakers']:
                         if call.data == speaker['id']:
-                            speaker_answer = bot.send_message(call.message.chat.id, text='Введите вопрос спикеру:')
-                            bot.register_next_step_handler(speaker_answer, confirm_send_message, speaker['speaker_id'])
-
-    if rexp := re.search(r'^id_(\d+)$', call.data):
-        answer_to_user = bot.send_message(call.message.chat.id, text='Введите ответ пользователю:')
-        # print(answer_to_user)
-        # print(confirm_send_message())
-        # print(rexp.group(1))
-        bot.register_next_step_handler(answer_to_user, confirm_send_message_user, rexp.group(1))
+                            speaker_question = bot.send_message(call.message.chat.id, text='Введите вопрос спикеру:')
+                            bot.register_next_step_handler(speaker_question, confirm_send_message, speaker['speaker_id'])
 
 
-def confirm_send_message(message: types.Message, speaker_id):
+def confirm_send_message(message: types.Message, speaker_id, role='speaker'):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button1 = types.KeyboardButton('Да')
     button2 = types.KeyboardButton('Нет')
     markup.add(button1, button2)
 
     confirmation = bot.send_message(message.chat.id, text='Отправить сообщение?', reply_markup=markup)
-    bot.register_next_step_handler(confirmation, send_message_to_speaker, message, speaker_id)
+    bot.register_next_step_handler(confirmation, send_message, message, speaker_id, role)
 
 
-def confirm_send_message_user(message: types.Message, speaker_id):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button1 = types.KeyboardButton('Да')
-    button2 = types.KeyboardButton('Нет')
-    markup.add(button1, button2)
-
-    confirmation = bot.send_message(message.chat.id, text='Отправить сообщение?', reply_markup=markup)
-    bot.register_next_step_handler(confirmation, send_message_to_user, message, speaker_id)
-
-
-def send_message_to_speaker(message, answer, chat_id):
+def send_message(message, answer, chat_id, role='speaker'):
     if message.text == 'Да':
         try:
             markup = types.InlineKeyboardMarkup()
-            markup.add(types.InlineKeyboardButton('Ответить', callback_data=f'id_{message.chat.id}'))
+            if role == 'speaker':
+                markup.add(types.InlineKeyboardButton('Ответить', callback_data=f'id_{message.chat.id}'))
 
             bot.send_message(
                 chat_id,
@@ -219,35 +189,53 @@ def send_message_to_speaker(message, answer, chat_id):
         )
 
 
-def send_message_to_user(message, answer, chat_id):
-    if message.text == 'Да':
+def pay_donate(message, amount):
+    if amount.isdigit():
+        amount = 100 * int(amount)
+    elif amount == 'user':
         try:
-            markup = types.InlineKeyboardMarkup()
-            # markup.add(types.InlineKeyboardButton('Ответить', callback_data=f'id_{message.chat.id}'))
-
-            bot.send_message(
-                chat_id,
-                answer.text,
-                reply_markup=markup
-            )
+            amount = 100 * int(message.text)
+        except ValueError:
             bot.send_message(
                 message.chat.id,
-                'Сообщение отправлено',
-                reply_markup=types.ReplyKeyboardRemove()
+                'Сумма введена неокрректно',
+                parse_mode='Markdown',
             )
-        except ApiTelegramException:
-            bot.send_message(
-                message.chat.id,
-                'Ошибка сервера. \nПожалуйста, повторите позднее.',
-                reply_markup=types.ReplyKeyboardRemove()
-            )
+            return
 
-    elif message.text == 'Нет':
-        bot.send_message(
-            message.chat.id,
-            'Сообщение не отправлено',
-            reply_markup=types.ReplyKeyboardRemove()
-        )
+    markup = types.InlineKeyboardMarkup()
+
+    markup.add(
+        types.InlineKeyboardButton('Отправить', pay=True),
+        types.InlineKeyboardButton('Назад', callback_data='donate'),
+    )
+
+    prices = [LabeledPrice(label='На развитие', amount=amount)]
+    bot.send_invoice(
+        chat_id=message.chat.id,
+        title='На развитие бота',
+        description='Вы донатите на развитие нашего бота и нашего бесплатного MeetUP',
+        invoice_payload='HAPPY FRIDAYS COUPON',
+        provider_token=provider_token,
+        currency='rub',
+        prices=prices,
+        is_flexible=False,
+        start_parameter='time-machine-example',
+        reply_markup=markup,
+    )
+
+    @bot.pre_checkout_query_handler(func=lambda query: True)
+    def checkout(pre_checkout_query):
+        bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True,
+                                      error_message="Попробуйте еще раз оплатить через несколько минут.")
+
+    @bot.message_handler(content_types=['successful_payment'])
+    def got_payment(message):
+        bot.send_message(message.chat.id,
+                         'Спасибо за ваш донат! Ваше пожертвование в размере `{} {}` пойдет на добрые дела! '
+                         .format(
+                             message.successful_payment.total_amount / 100, message.successful_payment.currency),
+                          parse_mode='Markdown')
 
 
 bot.infinity_polling()
